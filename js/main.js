d024ca3add133cdf3415fcf6212781e3379d3918
jonathanbosson <jonathan.bosson@gmail.com>
2016-02-19 09:28:09 +0100

    Now ignores data directory and mac DS_Store, new directory for topojson

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var area1;
 var map1;
 
-d3.csv("data/taxi_sthlm_march_2013_2.csv", function (data) {
+d3.csv("data/taxi_sthlm_march_2013_1.csv", function (data) {
 
 	//console.log(data)
     area1 = new area(data);
